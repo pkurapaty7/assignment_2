4da3ba4d121e7ac768a72c27db0ec17d08eb8a31
pkurapaty7 <93104483+pkurapaty7@users.noreply.github.com>
2022-02-06 17:41:59 -0800

    Add files via upload (#3)

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 var express = require('express')
 var app = express()
 const bodyParser = require('body-parser')
-var cors = require('cors')
+
 app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
+
 
 app.listen(3000)
 console.log('Node.js Express server is running on port 8000..')
@@ -14,14 +14,17 @@ app.get('/v1/hello', get_hello)
 app.post('/v1/auth', post_auth)
 
 function get_weather_v1(request, response){
+  if(request.headers.authorization == 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IlByZWV0aGkiLCJpYXQiOjE1MTYyMzkwMjJ9.BlWCMZG2SHus_FHOh7qO7tYjaF1em--UGT9D63p47cU')
     response.json({"coord":{"lon":-123.262,"lat":44.5646},"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01n"}],"base":"stations","main":{"temp":282.61,"feels_like":282.61,"temp_min":280.58,"temp_max":285.29,"pressure":1018,"humidity":84},"visibility":10000,"wind":{"speed":0.89,"deg":225,"gust":0.89},"clouds":{"all":0},"dt":1642038331,"sys":{"type":2,"id":2040223,"country":"US","sunrise":1642002454,"sunset":1642035291},"timezone":-28800,"id":5720727,"name":"Corvallis","cod":200})
 }
 function get_hello(request, response){
+  if(request.headers.authorization == 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IlByZWV0aGkiLCJpYXQiOjE1MTYyMzkwMjJ9.BlWCMZG2SHus_FHOh7qO7tYjaF1em--UGT9D63p47cU')
      response.json( {"message":"Good morning"} )
 }
 const fs = require("fs");
 
 function post_auth(req,res){
+  
     
     let usernames = ['nithish','sunny','kohli']
     let passwords = ['123','281','100']
